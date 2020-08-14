125dab23cf2ba846caf3f119cb647bf00b8b7a83
vikram <vikramsingh16294@gmail.com>
2020-08-14 17:28:30 +0530

    login validations

--- src/components/LoginComponent.js
+++ src/components/LoginComponent.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import LoginContainer from '../containers/LoginContainer';
+// import LoginContainer from '../containers/LoginContainer';
 import { CardBody, Container, Row, Col, Card, Form, FormGroup, Label, Input, Button, FormFeedback } from 'reactstrap';
 import PropTypes from 'prop-types';
-let LoginComponent = ({ username, password, setUsername, setPassword, logValues }) => {
+let LoginComponent = ({ username, password, setUsername, setPassword, logValues, schema }) => {
     const setUsernameWrapper = (evt) => {
         console.log(evt.target.value)
         setUsername(evt.target.value)
@@ -11,7 +11,14 @@ let LoginComponent = ({ username, password, setUsername, setPassword, logValues 
         setPassword(evt.target.value)
         console.log(evt.target.value)
     };
+    const showError = () => {
+        schema.validate({username: username, password: password}, {abortEarly: false})
+            .catch( (err) => {
+                console.log(err.errors[0]);
+            });
+    }
     return (
+        <>
         <Container>
             <Row>
                 <Col className="m-auto">
@@ -28,13 +35,15 @@ let LoginComponent = ({ username, password, setUsername, setPassword, logValues 
                                     <Input type="password" name="password" id="examplePassword" onChange={setPasswordWrapper}></Input>
                                     <FormFeedback>Oh noes! that name is already taken</FormFeedback>
                                 </FormGroup>
-                                <Button onClick={logValues} >Login</Button>
+                                <Button onClick={ () => {logValues(); showError();} } >Login</Button>
                             </Form>
                         </CardBody>
                     </Card>
                 </Col>
             </Row>
         </Container>
+        <p id= '1'></p>
+        </>
     );
 };
 
--- src/containers/LoginContainer.js
+++ src/containers/LoginContainer.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import LoginComponent from '../components/LoginComponent'
+import * as yup from 'yup';
 
 const LoginContainer = () => {
     const [username, setUsername] = useState('');
@@ -9,8 +10,14 @@ const LoginContainer = () => {
         console.log(username);
         console.log(password);
     }
+
+    let schema = yup.object().shape({
+        username: yup.string().email().required(),
+        password: yup.string().required(),
+    });
+
     return (
-        <LoginComponent username = { username } password = { password } setUsername = {setUsername} setPassword = {setPassword } logValues = { logValues}/>
+        <LoginComponent username = { username } password = { password } setUsername = {setUsername} setPassword = {setPassword } logValues = { logValues} schema = { schema }/>
     );
 };
 
